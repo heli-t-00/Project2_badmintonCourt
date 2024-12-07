82a61ede6c38063261b32ac9021f8ba771c3a312
heli-t-00 <helen2to@gmail.com>
2024-12-07 13:29:56 +0000

    added buttons for court areas, added ids for the areas on court, conditional coloring of areas court play,

--- assets/javascript/court.js
+++ assets/javascript/court.js
@@ -1,15 +1,24 @@
 console.log("x");
-// Click to change color areas on court function
-function setAllAreas() {
+// Click to change color of areas on court function
+// added if/else conditional selection of area to change color using ID
+// eg. ALL singles court area has 1 in ID
+function setAllAreas(including) {
   Array.prototype.slice
     .call(document.getElementsByClassName("area"))
-    .forEach(
-        area=> {
-      area.style.background = "blue";
+    .forEach((area) => {
+      if (area.id.includes(including)) area.style.background = "blue";
+      else area.style.background = "red";
     });
 }
 
 // Move on click
-function clickOnArea(event,area){
-    console.log(`clickOnArea ${event.pageX}, ${event.pageY},${area.id}`)
+function clickOnArea(event, area) {
+  console.log(`clickOnArea ${event.pageX}, ${event.pageY},${area.id}`);
+}
+
+function showSinglesCourt() {
+  setAllAreas("1");
+}
+function showDoublesCourt() {
+  setAllAreas("");
 }
