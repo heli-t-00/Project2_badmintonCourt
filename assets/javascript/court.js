816307accbca98e2bc060d75b5fc214a217b153e
heli-t-00 <helen2to@gmail.com>
2024-12-11 20:44:56 +0000

    shuttle moves

--- assets/javascript/court.js
+++ assets/javascript/court.js
@@ -1,4 +1,3 @@
-
 // Click to change color of areas on court function
 // added if/else conditional selection of area to change color using ID
 // eg. ALL singles court area has 1 in ID
@@ -11,9 +10,42 @@ function setAllAreas(including) {
     });
 }
 
+function getAngleInDegrees(x, y) {
+  angle = Math.round(Math.atan2(y, x) * (180 / Math.PI));
+  return angle < 0 ? (angle += 360) : angle;
+}
+
+/* Move the shuttle to x,y and set the scale to s*/
+function moveShuttle(shuttle, x, y) {
+  console.log(
+    `Shuttle top=${shuttle.style.top}` +
+      ` left=${shuttle.style.left}` +
+      ` zoom=${shuttle.style.zoom}` +
+      ` transform=${shuttle.style.transform}`
+  );
+  o = shuttle.getBoundingClientRect(); /* old position */
+  console.log(`moveShuttle (${o.left},${o.top})`);
+  /* set the new position */
+  shuttle.style.top = `${y - o.height / 2}px`;
+  shuttle.style.left = `${x - o.width / 2}px`;
+  // shuttle.style.scale=`${s}%`;
+  shuttleImage = document.getElementById("shuttleimage");
+  shuttleImage.style.zoom = `${s}%`;
+  console.log(
+    `Shuttle top=${shuttle.style.top}` +
+      ` left=${shuttle.style.left}` +
+      ` zoom=${shuttle.style.zoom}` +
+      ` transform=${shuttle.style.transform}`
+  );
+}
+
 // Move on click
 function clickOnArea(event, area) {
   console.log(`clickOnArea ${event.pageX}, ${event.pageY},${area.id}`);
+  let shuttle = document.getElementById("shuttle");
+  if (area && shuttle) {
+    moveShuttle(shuttle, event.pageX, event.pageY);
+  }
 }
 
 function showSinglesCourt() {
@@ -23,12 +55,10 @@ function showDoublesCourt() {
   setAllAreas("");
 }
 
-
-
 // W3 school Animation CODE
 function myMove() {
   let id = null;
-  const elem = document.getElementById("animate");   
+  const elem = document.getElementById("animate");
   let pos = 0;
   clearInterval(id);
   id = setInterval(frame, 5);
@@ -36,26 +66,25 @@ function myMove() {
     if (pos == 350) {
       clearInterval(id);
     } else {
-      pos++; 
-      elem.style.top = pos + "px"; 
-      elem.style.left = pos + "px"; 
+      pos++;
+      elem.style.top = pos + "px";
+      elem.style.left = pos + "px";
     }
   }
 }
 
 // Allows court to appear and intro to fade out
 
-function showCourt (){
- court = document.getElementsByClassName ("court")[0];
- court.style.opacity = "100%";
- court.style.transition = "opacity 5s";
- intro = document.getElementsByClassName ("intro")[0];
- intro.style.display = "none";
- bar = document.getElementsByClassName ("bar")[0];
- bar.style.display ="flex";
- shuttle = document.getElementsByClassName ("shuttle");
- shuttle.style.display = "block";
-
+function showCourt() {
+  court = document.getElementsByClassName("court")[0];
+  court.style.opacity = "100%";
+  court.style.transition = "opacity 5s";
+  intro = document.getElementsByClassName("intro")[0];
+  intro.style.display = "none";
+  bar = document.getElementsByClassName("bar")[0];
+  bar.style.display = "flex";
+  shuttle = document.getElementById("shuttle");
+  shuttle.style.display = "block";
 }
 
 //  TRY to write a fade in fade out function using jQuery
@@ -75,28 +104,28 @@ function showCourt (){
 //   }
 // }
 
-const shuttle = document.getElementById("shuttle");
+// const shuttle = document.getElementById("shuttle");
 
-// Move the shuttle to the center of clicked area
-document.querySelectorAll(".area").forEach((area) => {
-  area.addEventListener("click", function () {
-    const rect = this.getBoundingClientRect();
-    shuttle.style.top = `${
-      rect.top + window.scrollY + rect.height / 2 - shuttle.offsetHeight / 2
-    }px`;
-    shuttle.style.left = `${
-      rect.left + window.scrollX + rect.width / 2 - shuttle.offsetWidth / 2
-    }px`;
-  });
-});
+// // Move the shuttle to the center of clicked area
+// document.querySelectorAll(".area").forEach((area) => {
+//   area.addEventListener("click", function () {
+//     const rect = this.getBoundingClientRect();
+//     shuttle.style.top = `${
+//       rect.top + window.scrollY + rect.height / 2 - shuttle.offsetHeight / 2
+//     }px`;
+//     shuttle.style.left = `${
+//       rect.left + window.scrollX + rect.width / 2 - shuttle.offsetWidth / 2
+//     }px`;
+//   });
+// });
 
-// Set initial shuttle position
-const initialArea = document.querySelector(".area");
-const rect = initialArea.getBoundingClientRect();
-shuttle.style.position = "absolute";
-shuttle.style.top = `${
-  rect.top + window.scrollY + rect.height / 2 - shuttle.offsetHeight / 2
-}px`;
-shuttle.style.left = `${
-  rect.left + window.scrollX + rect.width / 2 - shuttle.offsetWidth / 2
-}px`;
+// // Set initial shuttle position
+// const initialArea = document.querySelector(".area");
+// const rect = initialArea.getBoundingClientRect();
+// shuttle.style.position = "absolute";
+// shuttle.style.top = `${
+//   rect.top + window.scrollY + rect.height / 2 - shuttle.offsetHeight / 2
+// }px`;
+// shuttle.style.left = `${
+//   rect.left + window.scrollX + rect.width / 2 - shuttle.offsetWidth / 2
+// }px`;
