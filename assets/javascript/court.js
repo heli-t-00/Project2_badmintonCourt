627d3f2a723159e895c31a4f71ef393782e073f2
heli-t-00 <helen2to@gmail.com>
2024-12-11 21:17:31 +0000

    removed unused code in js file

--- assets/javascript/court.js
+++ assets/javascript/court.js
@@ -55,23 +55,7 @@ function showDoublesCourt() {
   setAllAreas("");
 }
 
-// W3 school Animation CODE
-function myMove() {
-  let id = null;
-  const elem = document.getElementById("animate");
-  let pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos + "px";
-      elem.style.left = pos + "px";
-    }
-  }
-}
+
 
 // Allows court to appear and intro to fade out
 
@@ -87,45 +71,3 @@ function showCourt() {
   shuttle.style.display = "block";
 }
 
-//  TRY to write a fade in fade out function using jQuery
-// function fn1(){
-//   $(".court").fadeToggle();
-// }
-
-// Shuttle Moving on court - to check
-// function moveOnClick(event, el) {
-//   let shuttle = document.getElementsByClassName("shuttle")[0];
-//   if (el && shuttle) {
-//     let bcr = el.getBoundingClientRect();
-//     let ibcr = shuttle.getBoundingClientRect();
-//     moveShuttle(shuttle, event.clientX - 25, event.clientY - 25, 0, 30);
-//     shuttle.style.transitionProperty = "all";
-//     shuttle.style.transitionDuration = "1s";
-//   }
-// }
-
-// const shuttle = document.getElementById("shuttle");
-
-// // Move the shuttle to the center of clicked area
-// document.querySelectorAll(".area").forEach((area) => {
-//   area.addEventListener("click", function () {
-//     const rect = this.getBoundingClientRect();
-//     shuttle.style.top = `${
-//       rect.top + window.scrollY + rect.height / 2 - shuttle.offsetHeight / 2
-//     }px`;
-//     shuttle.style.left = `${
-//       rect.left + window.scrollX + rect.width / 2 - shuttle.offsetWidth / 2
-//     }px`;
-//   });
-// });
-
-// // Set initial shuttle position
-// const initialArea = document.querySelector(".area");
-// const rect = initialArea.getBoundingClientRect();
-// shuttle.style.position = "absolute";
-// shuttle.style.top = `${
-//   rect.top + window.scrollY + rect.height / 2 - shuttle.offsetHeight / 2
-// }px`;
-// shuttle.style.left = `${
-//   rect.left + window.scrollX + rect.width / 2 - shuttle.offsetWidth / 2
-// }px`;
